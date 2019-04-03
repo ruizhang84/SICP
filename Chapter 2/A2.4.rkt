843d873e1a1899f07dc75347378f10a63f726de4
ruizhang84 <rzhang360@gatech.edu>
2019-04-03 00:25:46 -0400

    Add code

--- Chapter 2/A2.4.rkt
+++ Chapter 2/A2.4.rkt
@@ -108,7 +108,7 @@
 #;(define (make-from-mag-ang r a)
   (make-from-mag-ang-polar r a))
 
-(define (install-rectangular-package)
+#;(define (install-rectangular-package)
   ;; internal procedures
   (define (real-part z) (car z))
   (define (imag-part z) (cdr z))
@@ -131,7 +131,7 @@
   (put 'make-from-mag-ang 'rectangular
        (lambda (r a) (tag (make-from-mag-ang r a))))
   'done)
-(define (install-polar-package)
+#;(define (install-polar-package)
   ;; internal procedures
   (define (magnitude z) (car z))
   (define (angle z) (cdr z))
@@ -176,7 +176,7 @@
 
 
 
-(define (apply-generic op . args)
+#;(define (apply-generic op . args)
   (let ((type-tags (map type-tag args)))
     (let ((proc (get op type-tags)))
       (if proc
@@ -185,13 +185,13 @@
            "No method for these types: APPLY-GENERIC"
            (list op type-tags))))))
 
-(define (real-part z) (apply-generic 'real-part z))
-(define (imag-part z) (apply-generic 'imag-part z))
-(define (magnitude z) (apply-generic 'magnitude z))
-(define (angle z) (apply-generic 'angle z))
-(define (make-from-real-imag x y)
+#;(define (real-part z) (apply-generic 'real-part z))
+#;(define (imag-part z) (apply-generic 'imag-part z))
+#;(define (magnitude z) (apply-generic 'magnitude z))
+#;(define (angle z) (apply-generic 'angle z))
+#;(define (make-from-real-imag x y)
   ((get 'make-from-real-imag 'rectangular) x y))
-(define (make-from-mag-ang r a)
+#;(define (make-from-mag-ang r a)
   ((get 'make-from-mag-ang 'polar) r a))
 
 
@@ -248,6 +248,38 @@
   (put 'deriv '* make-product)
   'done)
 
-(install-deriv-package)
-(deriv '(+ x x 3) 'x)
+;(install-deriv-package)
+;(deriv '(+ x x 3) 'x)
 ;'2
+
+;2.75
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude) (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle) (atan y x))
+          (else (error "Unknown op: MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
+(define (apply-generic op arg) (arg op))
+(define (make-from-mag-ang x y)
+  (define (dispatch op)
+    (cond ((eq? op 'magnitude) x)
+          ((eq? op 'angle) y)
+          ((eq? op 'magnitude) (* x (cos y)))
+          ((eq? op 'angle) (* x (sin y)))
+          (else (error "Unknown op: MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
+
+
+
+
+
+
+
+
+
+
+
+
+
