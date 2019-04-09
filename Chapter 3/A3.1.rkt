20612539d4812e09542dd5984872feca092ce638
zhang <zhang@zhangdeiMac.home>
2019-04-08 23:06:20 -0400

    add code

--- Chapter 3/A3.1.rkt
+++ Chapter 3/A3.1.rkt
@@ -1,1 +1,65 @@
 #lang racket
+#;(define balance 100)
+#;(define (withdraw amount)
+  (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds"))
+
+(define new-withdraw
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount))
+                 balance)
+          "Insufficient funds"))))
+
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds")))
+
+
+;(define W1 (make-withdraw 100))
+;(W1 50)
+
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request: MAKE-ACCOUNT"
+                       m))))
+  dispatch)
+
+;(define acc (make-account 100))
+;((acc 'withdraw) 50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
